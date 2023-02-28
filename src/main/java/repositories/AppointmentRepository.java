fc3e8fd191f88d777caf57844b3699cff03c52c0
Saba Alshaeer <98250632+sabaalshaeer@users.noreply.github.com>
2023-02-28 01:09:40 -0600

    Update AppointmentRepository.java

--- src/main/java/repositories/AppointmentRepository.java
+++ src/main/java/repositories/AppointmentRepository.java
@@ -7,6 +7,10 @@ import com.example.demo.models.Appointment;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long>{
+	//write query that will find all appointments that has some matching PatientId/power of JPA
+	//JPA will uses this exact name and convert it to SQL query
+	//this query to return list or array of appointments , so we use Iterable class to denote this is going to be array,by PatientId with providing that id
+	Iterable<Appointment> findAllByPatientId(Long id);
 	
 
 }
